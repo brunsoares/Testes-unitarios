fcb10a0f15e63b19801efc99098ecbd65ff1e2c4
Bruno Soares <brunohenrique.soares@outlook.com>
2022-07-18 20:19:51 -0300

    Correção Sonar lint #Commit
    Alterações:
    - Correção sobre as exceções do sonar lint, criando exceções próprias sem afetar os testes;

---
+++ JUnit/src/main/java/br/com/brunsoares/exceptions/FilmeSemEstoqueException.java
@@ -0,0 +1,8 @@
+package br.com.brunsoares.exceptions;
+
+public class FilmeSemEstoqueException extends Exception{
+
+    public FilmeSemEstoqueException(String message) {
+        super(message);
+    }
+}
